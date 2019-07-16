13f7fd37bc2bd572332cdde8c92a1978a6d8e73f
Nicholas Fearns <you@example.com>
2019-07-16 23:43:03 +0000

    set up app to render correct component based on the state

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import Note from './components/Note';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
 render() {
+  const { showNote } = this.state;
+
   return (
     <div className="App">
     <Nav />
-    <List />
-    <Note />
+    { showNote ? <Note /> : <List /> }
     </div>
   );
 }
---
+++ src/tree.rb
@@ -0,0 +1,24 @@
+class Tree
+  attr_accessor :payload, :children
+
+  def initialize(payload, children)
+    @payload = payload
+    @children = children
+  end
+
+end
+
+
+# The "Leafs" of a tree, elements that have no children
+deep_fifth_node = Tree.new(5, [])
+eleventh_node = Tree.new(11, [])
+fourth_node   = Tree.new(4, [])
+
+# The "Branches" of the tree
+ninth_node = Tree.new(9, [fourth_node])
+sixth_node = Tree.new(6, [deep_fifth_node, eleventh_node])
+seventh_node = Tree.new(7, [sixth_node])
+shallow_fifth_node = Tree.new(5, [ninth_node])
+
+# The "Trunk" of the tree
+trunk   = Tree.new(2, [seventh_node, shallow_fifth_node])
