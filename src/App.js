eb699df10b2059e7a5ae5de5834d9449cced6083
Nicholas Fearns <you@example.com>
2019-07-20 23:27:34 +0000

    beginning set up of RESTful api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
 
 
 class App extends Component {
@@ -19,13 +20,19 @@ class App extends Component {
     });
   }
 
+  getNotes = () => {
+    axios.get('https://note-app-nick-fearns.herokuapp.com/notes')
+    .then((res) => console.log(res.data) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
 render() {
   const { toggleNote, showNote } = this.state;
 
   return (
     <div className="App">
     <Nav toggleNote={this.toggleNote} showNote={showNote} />
-    { showNote ? 'Cancel' : '+ Note' }
+    { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
     </div>
   );
 }
