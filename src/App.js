20759da951d7d40515cc968404ddc6db683c32e8
Nicholas Fearns <you@example.com>
2019-07-21 19:45:46 +0000

    pulling note data from the app component into the state

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      showNote: false
+      showNote: false,
+      notes: []
     };
   }
 
@@ -23,17 +24,23 @@ class App extends Component {
 
   getNotes = () => {
     axios.get(urlFor('notes'))
-    .then((res) => console.log(res.data) )
+    .then((res) => this.setState({ notes: res.data}) )
     .catch((err) => console.log(err.response.data) );
   }
 
 render() {
-  const { toggleNote, showNote } = this.state;
+  const { showNote, notes } = this.state;
 
   return (
     <div className="App">
     <Nav toggleNote={this.toggleNote} showNote={showNote} />
-    { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
+    { showNote ? 
+      <Note /> 
+      : <List
+       getNotes={this.getNotes}
+       notes={notes}
+       />
+      }
     </div>
   );
 }
