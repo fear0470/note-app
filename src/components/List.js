209f22763c7d376b8d725281b7355ca01cdf0b95
Nicholas Fearns <you@example.com>
2019-07-21 20:00:18 +0000

    set up the list component to loop through each note and display its subcomponents

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NoteCard from './NoteCard';
 
-class List extends React.Component {
+class List extends Component {
 
   componentWillMount() {
     this.props.getNotes();
   }
 
-
-
   render() {
+    const { notes } = this.props;
+
+    const cards = notes.map((note, index) => {
+      return (
+        <NoteCard
+          key={index}
+          index={index}
+          note={note}
+        />
+      );
+    });
+
     return (
       <div className="list-container">
-        List Component
+        {cards}
       </div>
     );
   }
 }
 
-export default List
+export default List;
