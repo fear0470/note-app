a2e0079765e917852d07f2a41d656aa53551aaa0
Nicholas Fearns <you@example.com>
2019-08-06 23:50:26 +0000

    syncing the tag to the note API when the form is submitted

--- src/App.js
+++ src/App.js
@@ -65,6 +65,12 @@ class App extends Component {
     this.setState({ newTag: false });
   }
 
+  submitTag = (data, noteId) => {
+    axios.post(urlFor(`notes/${noteId}/tags`), data)
+    .then((res) => this.getNote(noteId) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render() {
     const { showNote, notes, note, newTag } = this.state;
 
@@ -77,6 +83,7 @@ class App extends Component {
             submitNote={this.submitNote}
             showTagForm={this.showTagForm}
             closeTagForm={this.closeTagForm}
+            submitTag={this.submitTag}
           />
           :
           <List
