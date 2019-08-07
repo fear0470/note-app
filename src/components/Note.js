b7feadb139985e73aeac1c26a36c0e6e1828166c
Nicholas Fearns <you@example.com>
2019-08-07 22:51:49 +0000

    fixed error in note.js causing bug

--- src/components/Note.js
+++ src/components/Note.js
@@ -12,100 +12,93 @@ class Note extends React.Component {
   }
 
   onTagSubmit(e) {
-    e.preventDefault()  
+    e.preventDefault();
     const formData = {
       name: this.name.value
     };
     this.props.submitTag(formData, this.props.note.id)
     this.props.closeTagForm();
-  }
+  };
 
-
-  renderTagForm(note) {
+  renderTagForm (note) {
     if (note.id !== undefined) {
-    if (!this.props.newTag) {
-      return (
-        <span>
-          Tag your note:
-          <i
-            className="tag-button material-icons"  
-            onClick={() => this.props.showTagForm()}
-          >
-            add circle
-          </i>
-        </span>
-      );
-    } else {
-      return (
-        <form onSubmit = {(e) => this.onTagSubmit(e)}>
-          <input
-            className="tag-input"
-            type="text"
-            placeholder="Tag Name..."
-            ref={(input) => this.name = input}
-           />
-        </form>
+      if (!this.props.newTag) {
+        return (
+          <span>
+        Tag your note:
+        <i className='tag-button material-icons' onClick={() => this.props.showTagForm()}>
+          add circle
+        </i>
+      </span>
+        );
+      } else {
+        return (
+          <form onSubmit={(e) => this.onTagSubmit(e)}>
+            <input
+              className='tag-input'
+              type='text'
+              placeholder='Tag Name...'
+              ref={(input) => this.name = input}
+            />
+          </form>
+        )
+      }
+    }
+  };
+
+  renderTags(note) {
+    if (note.tags) {
+      return note.tags.map((tag, index) =>
+        <div
+          className='tag'
+          key={index}
+          onClick={(e) => this.props.deleteTag(note.id, tag.id)}
+        >
+          <span className='delete'>
+            <i className='material-icons'>delete</i>
+          </span>
+          {tag.name}
+        </div>
       );
     }
-  }
-}
+  };
 
 
-renderTags(note) {
-  if (note.tags) {
-    return note.tags.map((tag, index) =>
-      <div 
-        className="tag"
-        key={index}
-        onClick={(e) => this.props.deleteTag(note.id, tag.id)}
-        >
-        <span className="delete">
-          <i className="material-icons">delete</i>
-        </span>
-        {tag.name}
-      </div>
+  render () {
+    const { note, closeTagForm } = this.props;
+// TODO Ask Michael - how come for onClick we go onClick = function, but onSubmit is just rerenced?
+    return (
+            <div className='note-container'>
+              <form className="note-form"
+                    onSubmit={(e) => this.onSubmit(e)}
+                    onClick={() => closeTagForm()}
+              >
+                <input
+                        className="note-title-input"
+                        type="text"
+                        placeholder="Note Title..."
+                        defaultValue={note.title}
+                        ref = {(input) => this.title = input}
+                />
+                <textarea
+                  className='note-textarea'
+                  placeholder='Type Here ...'
+                  defaultValue={note.content}
+                  ref = {(input) => this.content = input }
+                />
+                <input className='note-button' type='submit' value='Submit' />
+              </form>
+              <div className='tag-container'>
+                <div className='tag-button-container'>
+                  {this.renderTagForm(note)}
+                </div>
+                <div className='tag-list-container'>
+                  {this.renderTags(note)}
+                </div>
+              </div>
+            </div>
     );
   }
-}
-
-
-
-render() {
-  const { note, closeTagForm } = this.props;
-
-  return (
-    <div className="note-container">
-      <form
-       className="note-form"
-       onSubmit={(e) => this.onSubmit(e)}
-       onClick={() => closeTagForm()}
-       >
-        <input
-          className="note-title-input"
-          type="text"
-          placeholder="Note Title..."
-          defaultValue={note.title}
-          ref={(input) => this.title = input}
-         />
-        <textarea
-          className="note-textarea"
-          placeholder="Type Here..."
-          defaultValue={note.content}
-          ref={(input) => this.content = input}
-        />
-        <input className="note-button" type="submit" value="Submit" />
-      </form>
-      <div className="tag-container">
-        <div className="tag-button-container">
-          {this.renderTagForm()}
-        </div>
-        <div className="tag-list-container">
-          {this.renderTags(note)}
-        </div>
-      </div>
-    </div>
-  );
- }
 }
 
 export default Note;
